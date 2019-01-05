69851397906c3b0e5b4f55780f50c6fe5751b95c
Robert Fischer <artiefischer@gmail.com>
2019-01-05 14:52:44 -0700

    php index being served

--- public/index.php
+++ public/index.php
@@ -7,17 +7,7 @@
 
 <body>
   <h1>GMRI Backend</h1>
-  <?php
-$r = new HttpRequest('/data', HttpRequest::METH_GET);
-try {
-    $r->send();
-    if ($r->getResponseCode() == 200) {
-        file_put_contents('local.rss', $r->getResponseBody());
-    }
-} catch (HttpException $ex) {
-    echo $ex;
-}
-?>
+<?php echo 'HI there'; ?>
 
 </body>
 
