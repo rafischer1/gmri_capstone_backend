40c71f9e810f594e735217651915ae2c2a0c591b
Robert Fischer <artiefischer@gmail.com>
2019-01-05 10:01:37 -0700

    arch setup

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
@@ -5,6 +6,7 @@ const logger = require('morgan');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
+const adminRouter = require('./routes/admin');
 
 const app = express();
 
@@ -14,7 +16,21 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(function(req, res, next) {
+  res.header(
+    "Access-Control-Allow-Origin",
+    "*"
+  );
+  res.header("Access-Control-Allow-Methods", "GET,POST,DELETE,PATCH,OPTIONS,PUT");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/admin', adminRouter);
 
 module.exports = app;
---
+++ sprint_planning.md
@@ -0,0 +1,17 @@
+# Planning and Layout for F.A.S. 🌊
+
+## To Do List 📋
+
+[ ] Testing Framework Working
+
+[ ] MVC
+
+[ ] For Web: Admin route with verification
+
+[ ] 'Users'route to store phone # and location
+
+[ ] 'Data' route to store flooding instance info for display in more info page and admin page
+
+[ ] Admin page needs: getALl users #, instance info
+
+[ ] Initialize DB "capstonedb" w/ seed data
