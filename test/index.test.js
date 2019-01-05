687c98fe06a4c43a5d8b492136910696a2055438
Robert Fischer <artiefischer@gmail.com>
2019-01-05 10:11:28 -0700

    test

--- test/index.test.js
+++ test/index.test.js
@@ -4,7 +4,7 @@ const expect = chai.expect
 const app = require('../app')
 const url = require('url')
 
-describe("GET /api", () => {
+describe("GET /", () => {
 
   it("renders a list of available endpoints", async function () {
     const response = await chai.request(app).get('/')
@@ -22,6 +22,9 @@ describe("GET /api", () => {
         },
         users: {
           href: `http://127.0.0.1:${port}/users`
+        },
+        admin: {
+          href: `http://127.0.0.1:${port}/admin`
         },
       }
     })
